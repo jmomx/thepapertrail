069c20fc6267270af4e6b2c1bb4fccc01629895a
John Morrow <me@mrjohnmorrow.com>
2015-04-13 09:41:27 +0200

    Publishing 4/13 site vn 3

--- scripts/city-data.js
+++ scripts/city-data.js
@@ -1,30 +1,57 @@
-var citytext = {"cities":[
+var cityarray = {"cities":[
   
-      {"name":"california", "videourl":""},
+      {"name":"bologna", "videourl":"http://player.vimeo.com/external/124641420.hd.mp4?s=db39db17d2804e38f4945b8e9c039853&profile_id=113",
+        "videourl":"http://player.vimeo.com/external/124641420.hd.mp4?s=db39db17d2804e38f4945b8e9c039853&profile_id=113"},
   
-      {"name":"chicago", "videourl":""},
+      {"name":"buffalo", "videourl":"http://player.vimeo.com/external/124722114.hd.mp4?s=df5e61be9912c638812af75b910c4449&profile_id=113",
+        "videourl":"http://player.vimeo.com/external/124722114.hd.mp4?s=df5e61be9912c638812af75b910c4449&profile_id=113"},
   
-      {"name":"colorado", "videourl":""},
+      {"name":"california", "videourl":"",
+        "videourl":""},
   
-      {"name":"czechrepublic", "videourl":""},
+      {"name":"chicago", "videourl":"",
+        "videourl":""},
   
-      {"name":"idaho", "videourl":""},
+      {"name":"colorado", "videourl":"",
+        "videourl":""},
   
-      {"name":"ireland", "videourl":""},
+      {"name":"czechrepublic", "videourl":"",
+        "videourl":""},
   
-      {"name":"japan", "videourl":""},
+      {"name":"idaho", "videourl":"",
+        "videourl":""},
   
-      {"name":"newcastleupontyne", "videourl":""},
+      {"name":"ireland", "videourl":"",
+        "videourl":""},
   
-      {"name":"northernlights", "videourl":""},
+      {"name":"japan", "videourl":"",
+        "videourl":""},
   
-      {"name":"pennsylvania", "videourl":""},
+      {"name":"newcastleupontyne", "videourl":"",
+        "videourl":""},
   
-      {"name":"seattle", "videourl":""},
+      {"name":"northernlights", "videourl":"",
+        "videourl":""},
   
-      {"name":"thecanaryislands", "videourl":""},
+      {"name":"pennsylvania", "videourl":"",
+        "videourl":""},
+  
+      {"name":"seattle", "videourl":"",
+        "videourl":""},
+  
+      {"name":"thecanaryislands", "videourl":"",
+        "videourl":""},
   
   {"name":"lastitem", "videourl":"lasturl"}
-]}
+]};
 
-var cityobj = JSON.parse(citytext)
+var cityarray = cityarray.cities;
+
+var urlmap = {}
+
+for (var i = 0; i < cityarray.length; i++) {
+  urlmap[cityarray[i].name] = cityarray[i].videourl;
+}
+
+
+//var cityobj = JSON.parse(citytext);
