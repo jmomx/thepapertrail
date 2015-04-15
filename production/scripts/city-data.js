64fc5873fea12722ad9af9687d4dba8a0bf16ba1
John Morrow <me@mrjohnmorrow.com>
2015-04-15 21:17:59 +0200

    Fixing bottom bar spacing and last url bug

--- production/scripts/city-data.js
+++ production/scripts/city-data.js
@@ -6,7 +6,7 @@ var cityarray = {"cities":[
       {"name":"{{ city.name }}", "videourl":"{{ city.videourl }}",
         "videourl":"{{ city.videourl }}"},
   {% endfor %}
-  {"name":"lastitem", "videourl":"lasturl"}
+  {"name":"lastitem", "videourl":"http://player.vimeo.com/external/124890315.sd.mp4?s=3c6b80a597bcce0c7b33e5c30642d0e5&profile_id=112"}
 ]};
 
 var cityarray = cityarray.cities;
